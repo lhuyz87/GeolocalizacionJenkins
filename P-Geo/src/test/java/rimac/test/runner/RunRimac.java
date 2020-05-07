fc7bf0cb015f5aa9125db8f339b37c4df1479de8
lhuyz87 <lhuyz87@gmail.com>
2020-05-07 01:31:09 -0500

    todos los casos comentados

--- P-Geo/src/test/java/rimac/test/runner/RunRimac.java
+++ P-Geo/src/test/java/rimac/test/runner/RunRimac.java
@@ -8,7 +8,7 @@ import rimac.test.util.ExcelUtilPropio;
 @RunWith(RunPersonalizar.class)
 
 //@CucumberOptions(features = { "src/test/resources/features/" }, tags = { "@CopiarPlantillas" }, glue = { "rimac" })
-@CucumberOptions(features = { "src/test/resources/features/" }, tags = {"@RegistrarNuevoCaso"}, glue = { "rimac" })
+//@CucumberOptions(features = { "src/test/resources/features/" }, tags = {"@RegistrarNuevoCaso"}, glue = { "rimac" })
 //@CucumberOptions(features = { "src/test/resources/features/" }, tags = {"@CopiarPlantillas"}, glue = { "rimac.test.definition" })
 //@CucumberOptions(features = { "src/test/resources/features/" }, tags = {"@RegistrarPlacaEnUso"}, glue = { "rimac" })
 //@CucumberOptions(features = { "src/test/resources/features/" }, tags = {"@AsignarProcurador"}, glue = { "rimac" })
